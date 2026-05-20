6946f8316cce8715efb2011b31242f6fedef0fd2
Fernando <fernando@Fernandos-MacBook-Air.local>
2026-05-20 14:55:54 +0530

    Update Day 3 - Data Structures

--- Day-03-Data-Structures/student_management.py
+++ Day-03-Data-Structures/student_management.py
@@ -1,9 +1,26 @@
-students = []
+# ----- Introduction to list -----#
+##Tuples are immutable data structures, meaning they cannot be changed.
 
-def add_student(name, age):
-    students.append({'name': name, 'age': age})
+students = ['Marta','Antonio']
+std_marks =[20,15]
+print("Hello", students[1], "Your marks is: ", std_marks[1])
+print("Hello", students[0], "Your marks is: ", std_marks[0])
 
-if __name__ == '__main__':
-    add_student('Alice', 21)
-    add_student('Bob', 22)
-    print('Students:', students)
+
+# ----- Introduction to tuples -----#
+## Tuples are immutable data structures, meaning they cannot be changed. They are defined using parentheses () instead of square brackets [].
+students_tuple = ('Marta','Antonio')
+std_marks_tuple =(20,15)
+print("Hello", students_tuple[1], "Your marks is: ", std_marks_tuple[1])
+print("Hello", students_tuple[0], "Your marks is: ", std_marks_tuple[0])    
+
+
+# ----- Introduction to dictionary -----#    
+## Dictionaries are mutable data structures that store key-value pairs. They are defined using curly braces {} and each key is separated from its value by a colon (:). The keys must be unique and immutable, while the values can be of any data type.
+
+students_dict = {
+    "name": "Miguel",
+    "marks": 90,
+    "age": 20,
+}
+print(students_dict)
